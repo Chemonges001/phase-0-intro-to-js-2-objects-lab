baa4842b8e0450226974acc518c4e72fc78cb7aa
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-06-04 12:16:19 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution in this file!
+const employee={
+    name: "Oscar Kiprop",
+    streetAddress: "11 Nairobi",
+
+}
+function updateEmployeeWithKeyAndValue(obj, key, value){
+  return {
+      ...obj, [key]: value;
+  }
+}
