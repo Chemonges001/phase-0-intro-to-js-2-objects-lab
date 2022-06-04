e620ec423ba5be34cc12a68478aec5a13150829d
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-06-04 12:28:36 +0300

    commit tree 2843b569422d6309911bb1bf40b732929d461374

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ const employee={
 }
 function updateEmployeeWithKeyAndValue(obj, key, value){
   return {
-      ...obj, [key]: value;
+      ...obj, [key]: value
   }
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+obj[key]= value
+return obj
+}
+function deleteFromEmployeeByKey(obj, key, value){
+    const newEmployee= {...obj}
+    delete newEmployee[key]
+    return newEmployee
+}
+function destructivelyDeleteFromEmployeeByKey(obj, key, value){
+    delete obj[key]
+    return obj
 }
